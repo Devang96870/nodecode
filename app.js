fd7012d3dbaa6dc5fa08114ebc3c3d9d5c8666d8
devang <devang@devangs-MacBook-Air.local>
2021-07-31 15:46:21 +0530

    test

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
-const PORT = process.env.PORT || 8081;
-app.use(morgan("dev"));
+// const morgan = require("morgan");
+// const PORT = process.env.PORT || 8081;
+// app.use(morgan("dev"));
 app.get("/", (req, res) => {
   res.status(200).json({ Message: "It Work" });
 });
