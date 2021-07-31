32592759adaeeac1fa09946b5e1ccdb215ea37ad
devang <devang@devangs-MacBook-Air.local>
2021-07-31 14:19:57 +0530

    test

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const PORT = 3000 || process.env.PORT;
-
+app.use(morgan("dev"));
+app.get("/", (req, res) => {
+  res.status(200).json({ Message: "It Work" });
+});
 app.listen(PORT, () => {
-  console.log(`Your Application Running on port${PORT}`);
+  console.log(`Your Application Running on port ${PORT}`);
 });
